20d684a559830c0f36e94b5fb66702eab841d459
blp4hi <Philipp.Blanke@de.bosch.com>
2018-06-27 08:40:31 +0200

    [fix] layout of fonts, emacs dotfiles
    Signed-off-by: blp4hi <Philipp.Blanke@de.bosch.com>

---
+++ emacs/lisp/init-theme.el
@@ -0,0 +1,26 @@
+(use-package material-theme
+  :ensure t)
+
+(setq philipp/themes '(material material-light))
+(setq philipp/themes-index 0)
+
+(defun philipp/cycle-theme ()
+  (interactive)
+  (setq philipp/themes-index (% (1+ philipp/themes-index) (length philipp/themes)))
+  (philipp/load-indexed-theme))
+
+(defun philipp/load-indexed-theme ()
+  (philipp/try-load-theme (nth philipp/themes-index philipp/themes)))
+
+(defun philipp/try-load-theme (theme)
+  (if (ignore-errors (load-theme theme :no-confirm))
+      (mapcar #'disable-theme (remove theme custom-enabled-themes))
+    (message "Unable to find theme file for ‘%s’" theme)))
+
+(philipp/load-indexed-theme)
+       
+(evil-leader/set-key
+  "L"  'philipp/cycle-theme)
+
+
+(provide 'init-theme)
